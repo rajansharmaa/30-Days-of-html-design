e1f39c3ab582ca1ac4b0dd6f3fd937ef0e47a45a
RAJAN KUMAR <81325812+rajansharmaa@users.noreply.github.com>
2021-04-16 23:29:59 +0530

    Add files via upload

---
+++ 01.3d-boxes-background/script.js
@@ -0,0 +1,18 @@
+alert("Hello Dear,Follow us on Instagram as @CODEBYTEZ to support us.");
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement('div')
+      box.classList.add('box')
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      boxesContainer.appendChild(box)
+    }
+  }
+}
+
+createBoxes()
